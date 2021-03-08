82658eea4246010973eda0f691b8d0283b6edd84
InzamZ <zhengyinzhen2001@icloud.com>
2021-03-08 17:31:19 +0800

    ABC190B AC code

---
+++ Atcoder/ABC190/B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,s,d,a,b;
+    bool flag=false;
+    cin>>n>>s>>d;
+    while (n--)
+    {
+        cin>>a>>b;
+        if (a<s&&b>d) {
+            flag=true;
+            break;
+        }
+    }
+    if (flag) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
