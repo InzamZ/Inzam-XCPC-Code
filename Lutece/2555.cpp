2f45d3c3e1fd01d3024f4651cc51afbab00492fd
InzamZ <zhengyinzhen2001@icloud.com>
2021-05-10 16:48:19 +0800

    Accepted

--- Lutece/2555.cpp
+++ Lutece/2555.cpp
@@ -3,87 +3,131 @@ using namespace std;
 const int maxn = 1e6 + 10;
 using ll = long long;
 
-int op[maxn][3], f[maxn], _rank[maxn];
 struct node {
-    int t, x;
-    node(int a, int b): t(a), x(b) {}
-};
-stack<node>s;
+    int fa, l, r;
+    int ls, rs, rank;
+} Tree[maxn * 20];
 
-int ifind(int x)
+int  rt[maxn * 2];
+int tot = 0, Flag = 0;
+int a, op, b, val;
+
+inline void copy (int x, int y)
 {
-    while (x != f[x])
-        x = f[x];
-    return x;
+    Tree[x].l = Tree[y].l;
+    Tree[x].r = Tree[y].r;
+    Tree[x].ls = Tree[y].ls;
+    Tree[x].rs = Tree[y].rs;
+    Tree[x].fa = Tree[y].fa;
+    Tree[x].rank = Tree[y].rank;
 }
 
-bool ifsame(int x, int y)
+inline int build(int l, int r)
 {
-    return ifind(x) == ifind(y);
-}
-
-void doop1(int x, int y, int t)
-{
-    x = ifind(x);
-    y = ifind(y);
-    if (x == y)
-        return ;
-    if (_rank[x] > _rank[y])
-        swap(x, y);
-    f[x] = y;
-    _rank[y] += _rank[x];
-    s.push(node(t, x));
-}
-
-void doop2(int x)
-{
-    int cnt;
-    while (!s.empty() && s.top().t >= x) {
-        f[s.top().x] = s.top().x;
-        s.pop();
+    int now = tot++;
+    Tree[now].l = l;
+    Tree[now].r = r;
+    if (l == r) {
+        Tree[now].fa = l;
+        Tree[now].rank = 1;
+        return now;
     }
-    if (s.empty())
-        cnt = 1;
-    else
-        cnt = s.top().t + 1;
-    for (int j = cnt; j <= x; ++j) {
-        if (op[j][0] == 1)
-            doop1(op[j][1], op[j][2], j);
-        else if (op[j][0] == 2)
-            doop2(op[j][1]);
-    }
+    int mid = (l + r) >> 1;
+    Tree[now].ls = build(l, mid);
+    Tree[now].rs = build(mid + 1, r);
+    return now;
 }
 
-void doop3(int x, int y)
+inline int find(int root, int x)
 {
-    if (ifsame(x, y))
-        cout << 1 << endl;
+    node now = Tree[root];
+    if (now.l == now.r)
+        return now.fa;
+    int mid = (now.l + now.r) >> 1;
+    if (x <= mid)
+        return find(now.ls, x);
     else
-        cout << 0 << endl;
+        return find(now.rs, x);
 }
 
+inline int getfa(int root, int x)
+{
+    int fa = find(root, x);
+    if (fa == x)
+        return x;
+    else
+        return getfa(root, fa);
+}
+
+inline int update(const int pre, int loc, int val)
+{
+    int now = tot++;
+    copy(now, pre);
+    if (Tree[now].l == Tree[now].r ) {
+        Tree[now].fa = val;
+        return now;
+    }
+    int mid = (Tree[now].l + Tree[now].r) >> 1;
+    if (loc <= mid)
+        Tree[now].ls = update(Tree[now].ls, loc, val);
+    else
+        Tree[now].rs = update(Tree[now].rs, loc, val);
+    return now;
+}
+
+inline int read()
+{
+    int X = 0;
+    bool flag = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-')
+            flag = 0;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        X = (X << 1) + (X << 3) + ch - '0';
+        ch = getchar();
+    }
+    if (flag)
+        return X;
+    return ~(X - 1);
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; ++i)
-        f[i] = i, _rank[i] = 1;
-    for (int i = 1; i <= m; ++i) {
-        cin >> op[i][0];
-        if (op[i][0] == 2)
-            cin >> op[i][1];
-        else
-            cin >> op[i][1] >> op[i][2];
-    }
-    for (int i = 1; i <= m; ++i) {
-        if (op[i][0] == 1)
-            doop1(op[i][1], op[i][2], i);
-        else if (op[i][0] == 2)
-            doop2(op[i][1]);
-        else if (op[i][0] == 3)
-            doop3(op[i][1], op[i][2]);
+    int N, M;
+    N = read();
+    M = read();
+    rt[0] = build(1, N);
+    for (int i = 1; i <= M; ++i) {
+        op = read();
+        a = read();
+        if (op == 2) {
+            rt[i] = tot++;
+            copy(rt[i], rt[a]);
+        }
+        else if (op == 1) {
+            b = read();
+            int fa = getfa(rt[i - 1], a);
+            int fb = getfa(rt[i - 1], b);
+            if (Tree[fa].rank > Tree[fb].rank)
+                swap(fa, fb);
+            Flag = Tree[fa].rank == Tree[fb].rank;
+            Tree[fb].rank += Flag;
+            rt[i] = update(rt[i - 1], fa, fb);
+        }
+        else  {
+            b = read();
+            rt[i] = tot++;
+            copy(rt[i], rt[i - 1]);
+            int fa = getfa(rt[i], a), fb = getfa(rt[i], b);
+            if (fa == fb)
+                putchar('1'), putchar('\n');
+            else
+                putchar('0'), putchar('\n');
+        }
     }
     return 0;
 }
