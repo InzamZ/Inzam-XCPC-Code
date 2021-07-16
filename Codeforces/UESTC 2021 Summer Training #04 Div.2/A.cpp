db976d8a6fc0285920131480fe66df6601da06bb
InzamZ <zhengyinzhen2001@icloud.com>
2021-07-16 23:02:06 +0800

    Accepted

--- Codeforces/UESTC 2021 Summer Training #04 Div.2/A.cpp
+++ Codeforces/UESTC 2021 Summer Training #04 Div.2/A.cpp
@@ -5,40 +5,33 @@ using namespace std;
 #define f(a,b,c) for(int (a)=(b);(a)>=(c);--(a))
 
 using ll = long long;
-#define pii pair<int,int>
-#define pdd pair<double,double>
 
 const double pi = acos(-1.0);
 const int maxn = 2e3 + 10;
-const int inf = 1e9;
-const ll Inf = 1e18;
 
-// struct point {
-//     double x, y;
-// } p[maxn];
-
-pdd p[maxn];
+struct point {
+    double x, y;
+} p[maxn];
 
 double w[maxn];
 int cnt = 0;
-int T = 1, n, a, b;
+int n;
 
 int main()
 {
     scanf("%d", &n);
     ll ans = 0;
     for (int i = 1; i <= n; ++i)
-        scanf("%lf %lf", &p[i].first, &p[i].second);
+        scanf("%lf %lf", &p[i].x, &p[i].y);
     for (int i = 1; i <= n; ++i) {
         cnt = 0;
         for (int j = 1; j <= n; ++j) {
             if (j != i)
-                w[++cnt] = atan2(p[j].second - p[i].second, p[j].first - p[i].first);
+                w[++cnt] = atan2(p[j].y - p[i].y, p[j].x - p[i].x);
         }
         sort(w + 1, w + 1 + cnt);
-        for (int j = 1; j <= cnt && w[j] <= 0; ++j) {
-            int k = 1;
-            while (w[k] - w[j] < pi && k <= cnt)
+        for (int j = 1, k = 1; j <= cnt && w[j] <= 0; ++j) {
+            while (w[k] - w[j] <= pi && k <= cnt)
                 ++k;
             ans += (1ll * (k - j - 1) * (k - j - 2) / 2) * ((cnt - k + j) * (cnt - k + j - 1) / 2);
         }
