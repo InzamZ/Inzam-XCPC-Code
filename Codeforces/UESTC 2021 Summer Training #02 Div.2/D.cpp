0b6a3ac8cd0792909eb10d143e70dfe44674892e
InzamZ <zhengyinzhen2001@icloud.com>
2021-07-16 09:38:25 +0800

    Accepted

--- Codeforces/UESTC 2021 Summer Training #02 Div.2/D.cpp
+++ Codeforces/UESTC 2021 Summer Training #02 Div.2/D.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 using ll = long long;
 
-const int maxn = 1e5 + 5;
+const int maxn = 2e5 + 5;
+const int randk = 100;
 
 struct edge {
-    int u, v, next;
+    int  v, next;
 } e[maxn];
 
 int vis[maxn];
@@ -15,23 +16,27 @@ queue<int>q;
 int newbeg, cnt = 0;
 
 int fir[maxn], cur = 0, ans = 0;
+int T = 1, n, m, a, b;
+
 void addedge(int u, int v)
 {
-    e[++cur].u = u;
-    e[cur].v = v;
+    e[++cur].v = v;
     e[cur].next = fir[u];
     fir[u] = cur;
 }
 
 void bfs(int rt)
 {
+    for (int i = 1; i <= n; ++i)
+        vis[i] = -1;
     q.push(rt);
+    vis[rt] = 0;
     while (!q.empty()) {
         int res = q.front();
         q.pop();
         cnt++;
         for (int i = fir[res]; i; i = e[i].next) {
-            if (!vis[e[i].v]) {
+            if (vis[e[i].v] == -1) {
                 q.push(e[i].v);
                 vis[e[i].v] = vis[res] + 1;
                 if (vis[e[i].v] > ans)
@@ -43,36 +48,30 @@ void bfs(int rt)
 
 int main()
 {
-    int T = 1, n, m, a, b;
-    bool flag = true;
     // scanf("%d", &T);
+    srand(time(NULL));
+    rand();
     while (T--) {
-        scanf("%d %d", &m, &n);
-        for (int t = 1; t <= n; ++t) {
+        scanf("%d %d", &n, &m);
+        for (int t = 1; t <= m; ++t) {
             scanf("%d %d", &a, &b);
             addedge(a, b);
             addedge(b, a);
         }
         cnt = 0;
-        for (int t = 1; t <= m; ++t)
-            vis[t] = 0;
-        bfs(m);
-        if (cnt < m) {
-            printf("-1\n"); return 0;
-        }
-        else {
-            queue<int>p;
-            for (int t = 1; t <= m; ++t)
-                if (vis[t] = ans)
-                    p.push(t);
-            while (!p.empty()) {
-                for (int t = 1; t <= m; ++t)
-                    vis[t] = 0;
-                bfs(p.front());
-                p.pop();
+        for (int t = 1; t <= randk; ++t) {
+            int res = (1ll * rand() * rand()) % n + 1;
+            bfs(res);
+            if (cnt < n) {
+                printf("-1\n");
+                return 0;
             }
-            printf("%d\n", ans - 1);
+            cnt = 0;
         }
+        int k = 0;
+        while (ans > (1 << k))
+            ++k;
+        printf("%d\n", k + 1);
     }
     return 0;
 }
