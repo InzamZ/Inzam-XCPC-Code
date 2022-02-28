3debf46532f98e501191745d974e2ee5fe1292af
InzamZ <zhengyinzhen2001@icloud.com>
2022-02-28 23:57:10 +0800

    :purple_circle: Runtime Error!

---
+++ Codeforces/CF1529_R722/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define maxn 1000010
+#define F(a,b,c) for(int (a)=(b);(a)<=(c);++(a))
+#define f(a,b,c) for(int (a)=(b);(a)>=(c);--(a))
+#define INF 0x3f3f3f3f
+
+int num[maxn];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int T, n, mx, cnt = 0, x;
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        cnt = 1;
+        if (n == 1) {
+            cout << 1 << '\n';
+            continue;
+        }
+        for (int i = 1; i <= n; ++i)
+            cin >> num[i];
+        sort(num + 1, num + 1 + n);
+        for (int i = 2; i <= n; i++) {
+            if (i == 2)
+                mx = abs(num[2] - num[1]);
+            else
+                mx = min(mx, abs(num[i] - num[i - 1]));
+            if (mx >= num[i])
+                ++cnt;
+            else
+                break;
+        }
+        cout << cnt << '\n';
+    }
+    return 0;
+}
