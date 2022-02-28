cb038fc5677db9ce0279eb3bb3f0f52414310f56
InzamZ <zhengyinzhen2001@icloud.com>
2022-03-01 00:02:31 +0800

    :purple_circle: Runtime Error!

---
+++ Codeforces/CF1553_Harbour_Space_Scholarship_Contest_2021-2022/D.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define F(a,b,c) for(int (a)=(b);(a)<=(c);++(a))
+#define f(a,b,c) for(int (a)=(b);(a)>=(c);--(a))
+
+const int maxn = 1e3 + 10;
+const int maxb = 110;
+const int inf = 1e9;
+const ll Inf = 1e18;
+
+// inline char nc()
+// {
+//     static char buf[100000], *p1 = buf, *p2 = buf;
+//     return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
+// }
+// inline int read()
+// {
+//     char ch = nc(); int sum = 0;
+//     while (!(ch >= '0' && ch <= '9'))
+//         ch = nc();
+//     while (ch >= '0' && ch <= '9')
+//         sum = sum * 10 + ch - 48, ch = nc();
+//     return sum;
+// }
+
+char s1[maxn], s2[maxn];
+int ans = 10;
+
+int main()
+{
+    int T = 1, n, i;
+    scanf("%d", &T);
+    while (T--) {
+        bool flag = false;
+        scanf("%s", s1);
+        scanf("%s", s2);
+        int delta = strlen(s1) - strlen(s2), cur = 0;
+        if (delta >= 0)
+            for (int i = delta % 2; i < strlen(s1) && cur < strlen(s2); ) {
+                if (s1[i] != s2[cur])
+                    i += 2;
+                else ++cur, ++i;
+            }
+        if (cur == strlen(s2))
+            flag = true;
+        printf("%s\n", flag ? "Yes" : "No");
+    }
+    return 0;
+}
