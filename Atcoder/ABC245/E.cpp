96566396b4c3d60570185c7ce1b8c27f8bec3c43
InzamZ <zhengyinzhen2001@icloud.com>
2022-03-27 09:06:26 +0800

    :green_circle: Accepted!

---
+++ Atcoder/ABC245/E.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define REG register
+#define FREAD 1
+#define FIO std::ios::sync_with_stdio(false);\
+            std::cin.tie(0);
+#define FFF freopen("/out.txt" , "w", stdout);
+
+const int maxn = 1e6;
+const int maxb = 110;
+const int inf = 1e9;
+const ll Inf = 1e18;
+const ll mod = 1e9 + 7;
+
+#ifdef FREAD
+inline char nc()
+{
+    static char buf[100000], *p1 = buf, *p2 = buf;
+    return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? EOF : *p1++;
+}
+template<typename T>
+inline T read(T &x)
+{
+    x = 0;
+    T w = 1;
+    char ch = 0;
+    while (ch < '0' || ch > '9') {  // ch 不是数字时
+        if (ch == '-')
+            w = -1;        // 判断是否为负
+        ch = nc();               // 继续读入
+    }
+    while (ch >= '0' && ch <= '9') {  // ch 是数字时
+        x = x * 10 + (ch - '0');  // 将新读入的数字’加’在 x 的后面
+        // x 是 int 类型，char 类型的 ch 和 ’0’ 会被自动转为其对应的
+        // ASCII 码，相当于将 ch 转化为对应数字
+        // 此处也可以使用 (x<<3)+(x<<1) 的写法来代替 x*10
+        ch = nc();  // 继续读入
+    }
+    x *= w;
+    return x;  // 数字 * 正负号 = 实际数值
+}
+
+template<typename T>
+inline void write(T x)
+{
+    static int sta[35];
+    int top = 0;
+    do {
+        sta[top++] = x % 10, x /= 10;
+    }
+    while (x);
+    while (top)
+        putchar(sta[--top] + 48);  // 48 是 '0'
+}
+#endif
+int T = 1, n, ans = 0, m;
+
+multiset<int>S;
+
+struct inode {
+    int type;
+    int w, h;
+    bool operator < (const inode &rhs) const
+    {
+        if (w == rhs.w) {
+            if (type == rhs.type)
+                return h < rhs.h;
+            return type < rhs.type;
+        }
+        return w < rhs.w;
+    }
+};
+
+vector<inode>v;
+
+int main()
+{
+    // FIO;
+    // read(T);
+    while (T--) {
+        ans = 1;
+        S.clear();
+        read(n); read(m);
+        v.resize(n + m);
+        v.clear();
+        for (int i = 0; i < n; ++i) {
+            v[i].type = 0;
+            read(v[i].w);
+        }
+        for (int i = 0; i < n; ++i) {
+            v[i].type = 0;
+            read(v[i].h);
+        }
+        for (int i = n; i < n + m; ++i) {
+            v[i].type = 1;
+            read(v[i].w);
+        }
+        for (int i = n; i < n + m; ++i) {
+            v[i].type = 1;
+            read(v[i].h);
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < m + n; ++i) {
+            if (!v[i].type)
+                S.insert(v[i].h);
+            else {
+                auto tmp = S.lower_bound(v[i].h);
+                if (tmp != S.end())
+                    S.erase(tmp);
+                else {
+                    printf("No\n");
+                    exit(0);
+                }
+            }
+        }
+        printf("Yes\n");
+    }
+    return 0;
+}
