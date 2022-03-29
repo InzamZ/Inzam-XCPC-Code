07bafd45d66dd7d21145a737b0c20bc75451f3a1
InzamZ <zhengyinzhen2001@icloud.com>
2022-03-29 15:25:46 +0800

    :bug: 修复公式渲染失败的bug

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -35,79 +35,73 @@ module.exports = {
         search: false,
         smoothScroll: true,
         algolia: {
-          apiKey: '8eba1a9f0dbb348259599150575e76b2',
-          indexName: 'acmdocs',
-          // If Algolia did not provided you any `appId`, use `BH4D9OD16A` or remove this option
-          appId: 'TINIQMJEJ6',
+            apiKey: '8eba1a9f0dbb348259599150575e76b2',
+            indexName: 'acmdocs',
+            // If Algolia did not provided you any `appId`, use `BH4D9OD16A` or remove this option
+            appId: 'TINIQMJEJ6',
         },
         nav: [{
-            text: 'Guide',
-            link: '/guide/',
-        },
-        {
-            text: 'Atcoder',
-            link: '/atcoder/'
-        },
-        {
-            text: 'Codeforces',
-            link: '/codeforces/'
-        },
-        {
-            text: 'Languages',
-            ariaLabel: 'Language Menu',
-            items: [
-              { text: 'Chinese', link: '/language/chinese/' },
-              { text: 'Japanese', link: '/language/japanese/' }
-            ]
-          }
+                text: 'Guide',
+                link: '/guide/',
+            },
+            {
+                text: 'Atcoder',
+                link: '/atcoder/'
+            },
+            {
+                text: 'Codeforces',
+                link: '/codeforces/'
+            },
+            {
+                text: 'Languages',
+                ariaLabel: 'Language Menu',
+                items: [
+                    { text: 'Chinese', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
         ],
         sidebar: {
-            '/guide/': [
-                {
-                    title: 'XCPC题解',
-                    collapsable: false,
-                    children: [
-                        '',
-                        'using-vue',
-                    ]
-                }
-            ],
-            '/atcoder/': [
-                {
-                    title: 'Atcoder',
-                    collapsable: false,
-                    children: [
-                        '',
-                        'ABC182',
-                        'ABC188',
-                        'ABC189',
-                        'ABC244',
-                        'ABC245',
-                        'ARC106',
-                        'ARC116',
-                        'EducationDPContest',
-                        'keyence2021',
-                    ]
-                }
-            ],
-            '/codeforces/': [
-                {
-                    title: 'Guide',
-                    collapsable: false,
-                    children: [
-                        '',
-                        'CF1440_R684',
-                        'CF1451_R685',
-                        'CF1452_EDU98',
-                        'CF1459_R691',
-                        'CF1462_R690',
-                        'CF1463_EDU100',
-                        'CF1609-DR-Aut2021',
-                        'CF1613-Edu118',
-                        'CF1651-Edu124',
-                    ]
-                }
-            ],
+            '/guide/': [{
+                title: 'XCPC题解',
+                collapsable: false,
+                children: [
+                    '',
+                    'using-vue',
+                ]
+            }],
+            '/atcoder/': [{
+                title: 'Atcoder',
+                collapsable: false,
+                children: [
+                    '',
+                    'ABC182',
+                    'ABC188',
+                    'ABC189',
+                    'ABC244',
+                    'ABC245',
+                    'ARC106',
+                    'ARC116',
+                    'EducationDPContest',
+                    'keyence2021',
+                ]
+            }],
+            '/codeforces/': [{
+                title: 'Guide',
+                collapsable: false,
+                children: [
+                    '',
+                    'CF1440_R684',
+                    'CF1451_R685',
+                    'CF1452_EDU98',
+                    'CF1459_R691',
+                    'CF1462_R690',
+                    'CF1463_EDU100',
+                    'CF1609-DR-Aut2021',
+                    'CF1613-Edu118',
+                    'CF1651-Edu124',
+                ]
+            }],
         },
     },
     /**
@@ -116,5 +110,6 @@ module.exports = {
     plugins: [
         '@vuepress/plugin-back-to-top',
         '@vuepress/plugin-medium-zoom',
+        '@renovamen/vuepress-plugin-katex',
     ]
 }
