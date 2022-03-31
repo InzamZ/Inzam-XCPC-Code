370c7a2c201c21a1d0005b7818522e12d63b36d1
InzamZ <73429001+InzamZ@users.noreply.github.com>
2022-03-31 11:17:09 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -21,6 +21,20 @@ module.exports = {
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
     ],
 
+    locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/us/': {
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      title: 'VuePress',
+      description: 'Vue-powered Static Site Generator'
+    },
+    '/': {
+      lang: 'zh-CN',
+      title: 'VuePress',
+      description: 'Vue 驱动的静态网站生成器'
+    }
+  },
     /**
      * Theme configuration, here is the default theme configuration for VuePress.
      *
