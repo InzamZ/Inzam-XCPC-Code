772d35d920ae789ab9c4254b9e63ba4288ace2c0
InzamZ <me@inzamz.top>
2022-04-30 19:25:41 +0800

    :arrow_up: 更新网络流模板

--- Template/MaxFlow.cpp
+++ Template/MaxFlow.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int inf = 0x7fffffff >> 1;
+#define int long long
 class dinic
 {
 public:
-    int s, t;
+    int s, t, n, m;
     static const int maxn = 100010;
     static const int dmaxn = 10010;
+    // static const int INF = 0x7fffffff;
+    static const int inf = 0x7fffffffffffffff;
     vector<int>f[dmaxn];
     bool vis[dmaxn];
-    int cur[dmaxn],d[dmaxn];
+    int cur[dmaxn], d[dmaxn];
     struct edge {
         int to, cap, flow;
     };
@@ -55,7 +57,7 @@ public:
     }
     /**
      * @brief dfs寻找增广路，可以多次寻找
-     * 
+     *
      * @param x 当前所在的节点
      * @param a 当前的流量
      * @return 本次增广路得到的流量
@@ -79,7 +81,7 @@ public:
         return flow;
     }
     /**
-     * @brief 求最大流，要求事先完成读入 
+     * @brief 求最大流，要求事先完成读入
      */
     int max_flow()
     {
@@ -90,27 +92,79 @@ public:
         }
         return flow;
     }
-} Di;
-void fread(int &x)
+}DI;
+/**
+ * @param m 边数
+ * @param n 点数
+ * @param E 边的总集合
+ * @param e 连接每个节点的边的集合
+ * @param a 每次增广路过程中到达每个节点的最大流量
+ * @param p 每次增广路节每个点选择增广的边
+ */
+class EK
 {
-    x = 0; char s = getchar();
-    while (s < '0' || s > '9')
-        s = getchar();
-    while (s >= '0' && s <= '9') {
-        x = x * 10 + s - '0';
-        s = getchar();
+public:
+    struct edgeNode {
+        int from, to, cap, flow;
+        edgeNode(int _from, int _to, int _cap, int _flow): from(_from), to(_to), cap(_cap), flow(_flow) {}
+    };
+    int m, n, s, t;
+    static const int maxn = 100010;
+    // static const int INF = 0x7fffffff;
+    static const int INF = 0x7fffffffffffffff;
+    vector<edgeNode>E;
+    vector<int>e[maxn];
+    int a[maxn], p[maxn];
+    void addedge(int from, int to, int cap)
+    {
+        E.push_back(edgeNode{from, to, cap, 0});
+        E.push_back(edgeNode{to, from, 0, 0});
+        e[from].push_back(E.size() - 2);
+        e[to].push_back(E.size() - 1);
     }
-    return ;
-}
-int main()
+    int max_flow()
+    {
+        int flow = 0;
+        while (1) {
+            queue<int>q;
+            q.push(s);
+            for (int i = 0; i <= n; ++i)
+                a[i] = 0;
+            a[s] = INF;
+            while (!q.empty()) {
+                int rt = q.front(); q.pop();
+                for (int i = 0; i < e[rt].size(); ++i) {
+                    auto o = E[e[rt][i]];
+                    if (!a[o.to] && o.cap > o.flow) {
+                        a[o.to] = min(a[rt], o.cap - o.flow);
+                        p[o.to] = e[rt][i];
+                        q.push(o.to);
+                    }
+                }
+                if (a[t])
+                    break;
+            }
+            if (!a[t])
+                break;
+            for (int i = t; i != s; i = E[p[i]].from) {
+                E[p[i]].flow += a[t];
+                E[p[i] ^ 1].flow -= a[t];
+            }
+            flow += a[t];
+        }
+        return flow;
+    }
+};
+signed main()
 {
-    int n, m;
-    fread(n); fread(m); fread(Di.s); fread(Di.t);
-    for (int i = 1; i <= m; i++) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    cin >> DI.n >> DI.m >> DI.s >> DI.t;
+    for (int i = 1; i <= DI.m; i++) {
         int a, b, c;
-        fread(a); fread(b); fread(c);
-        Di.addedge(a, b, c);
+        cin >> a >> b >> c;
+        DI.addedge(a, b, c);
     }
-    printf("%d\n", Di.max_flow());
+    printf("%lld\n", DI.max_flow());
     return 0;
 }
