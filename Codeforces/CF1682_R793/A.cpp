c62077f0ece8c8ef32cc153d07ffe8ba208e3a94
InzamZ <zhengyinzhen2001@icloud.com>
2022-05-23 12:31:38 +0800

    :green_circle: Accepted!

---
+++ Codeforces/CF1682_R793/A.cpp
@@ -0,0 +1,46 @@
+//InzamZ
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int,int>
+#define f(i,a,b) for(int (i)=(a);(i)<=(b);(i)++)
+#define F(i,a,b) for(int (i)=(a);(i)>=(b);(i)--)
+#define FIO std::ios::sync_with_stdio(false);\
+            std::cin.tie(0)
+#define FFFOUT freopen("./out.txt" , "w", stdout)
+#define FFFIN freopen("./in.txt" , "r", stdin)
+const int maxn = 5e5 + 10;
+const int maxb = 110;
+int T = 1, n, m, x, ans = 0, a[maxn], b[maxn];
+vector<int>e[maxn],v;
+string s;
+
+int solve() {
+    ans = 0;
+    cin >> n >> s;
+    for (int i = n/2; i >= 0; i--) {
+        if (s[i] == s[n/2]) {
+            ++ans;
+        }
+        else break;
+    }
+    for (int i = n/2+1; i < n ;i++) {
+        if (s[i] == s[n/2]) {
+            ++ans;
+        }
+        else break;
+    }
+    cout << ans << '\n';
+    return 0;
+}
+
+signed main()
+{
+    FIO;
+    cin >> T;
+    while (T--) {
+    // cout << "Case #" << T + 1 << ":" << endl;
+        solve();
+    }
+    return 0;
+}
